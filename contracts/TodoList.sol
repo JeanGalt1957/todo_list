acfe430e112325fd0de19cb58a97be60d61012a4
JeanGalt1957 <matthew.f.gilbertson@gmail.com>
2021-02-05 18:43:18 -0500

    tests

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,8 +1,25 @@
 pragma solidity ^0.5.0;
 
 contract TodoList { //contract must be lower case!
-    uint public testCount = 0; // state variables are written to the blockchain
+    uint public taskCount = 0; // state variables are written to the blockchain
     // public allows taskCount can be read from contract
+
+    struct Task { //
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public { //herein is a default taks for testing
+        createTask("Check out this task");
+    }
+
+    function createTask(string memory _content) public { //puts the task inside of the Task struct mapping
+            taskCount ++;
+            tasks[taskCount] = Task(taskCount, _content, false); //1, 2, 3 correspend with struct data
+    }
 
 }
 
